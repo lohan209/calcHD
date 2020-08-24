000b627489157361ad49a0997c6f050789f003b6
lohan209 <36992808+lohan209@users.noreply.github.com>
2020-08-24 17:57:07 -0300

    Update mibocam3.php

--- calculadora/mibocam/mibocam3.php
+++ calculadora/mibocam/mibocam3.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <title>Calculadora de HD</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Baloo+Bhai&display=swap">
-    <link rel="stylesheet" type="text/css" href=../../../css/style.css>
+    <link rel="stylesheet" type="text/css" href=../css/style.css>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
